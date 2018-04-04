672d6ffc849a3aefa4101891f58194efeb1e0973
oonyoontong <oontong_tan@mymail.sutd.edu.sg>
2018-04-04 23:32:34 +0800

    nothing changed

--- src/components/live/LiveBox.vue
+++ src/components/live/LiveBox.vue
@@ -11,7 +11,7 @@
         LiveInput,
         LiveMessage
       },
-      
+       
 
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 // The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import VueGridLayout from 'vue-grid-layout'
-import Vue from 'vue'
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.import Vue from 'vue'
 import App from './App'
 import router from './router'
 import { store } from './store/index'
@@ -17,4 +15,4 @@ new Vue({
   components: {
     App
   }
-})
+});
